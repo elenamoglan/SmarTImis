57e0d6e3b659d27e69cae0660b2844887120ab5a
Helen <moglan.elena@gmail.com>
2026-03-29 18:55:54 +0300

    update layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
 
     return (
         <nav className="bg-white border-b border-gray-200 sticky top-0 z-[10000]">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
 
                     {/* Logo Section */}
